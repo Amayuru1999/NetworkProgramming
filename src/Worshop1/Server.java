dd4c46c4aa683a207550ff9b3e44ab0eac29bd36
Amayuru1999 <amarasingheau@gmail.com>
2024-02-16 08:58:52 +0530

    Initial

--- src/Worshop1/Server.java
+++ src/Worshop1/Server.java
@@ -3,7 +3,7 @@ package Worshop1;
 public class Server {
     public static void main(String[] args) {
         System.out.println("\t\t Simple Socket Server");
-        System.out.println("");
+        System.out.println("\t\t=======================\n\n");
 
     }
 }
